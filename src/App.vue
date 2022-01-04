0b12bfd5f2ac97f6fda02613db4041908da3285a
Ben Hong <ben@bencodezen.io>
2022-01-04 13:01:44 -0500

    feature: add whiteboard functionality for dragging titles around

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { useColorMode } from '@vueuse/core'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { useColorMode, useDraggable } from '@vueuse/core'
 import IconCafe from './components/IconCafe.vue'
 import IconMoon from './components/IconMoon.vue'
 import IconSun from './components/IconSun.vue'
@@ -9,6 +9,27 @@ const colorMode = useColorMode({
   modes: {
     cafe: 'cafe'
   }
+})
+
+const elNotImportantTitle = ref(null)
+const elImportantTitle = ref(null)
+const elNotUrgentTitle = ref(null)
+const elUrgentTitle = ref(null)
+
+const { style } = useDraggable(elNotImportantTitle, {
+  initialValue: { x: 40, y: 40 }
+})
+
+const { style: style2 } = useDraggable(elImportantTitle, {
+  initialValue: { x: 60, y: 60 }
+})
+
+const { style: style3 } = useDraggable(elNotUrgentTitle, {
+  initialValue: { x: 80, y: 80 }
+})
+
+const { style: style4 } = useDraggable(elUrgentTitle, {
+  initialValue: { x: 100, y: 100 }
 })
 
 const switchTheme = () => {
@@ -29,6 +50,8 @@ const switchTheme = () => {
       justify-content: space-between;
       height: 50px;
       align-items: center;
+      border-bottom: 1px solid #ccc;
+      padding-bottom: 10px;
     "
   >
     <h1 style="font-size: 1.2rem">Build with Ben #23</h1>
@@ -38,9 +61,31 @@ const switchTheme = () => {
       <IconCafe v-else />
     </button>
   </header>
-  <h1>{{ colorMode }}</h1>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main>
+    <h2>Whiteboard Canvas</h2>
+    <div class="whiteboard">
+      <div class="whiteboard-section">
+        <h3 ref="elNotImportantTitle" :style="style" style="position: fixed">
+          Not Important
+        </h3>
+      </div>
+      <div class="whiteboard-section">
+        <h3 ref="elImportantTitle" :style="style2" style="position: fixed">
+          Important
+        </h3>
+      </div>
+      <div class="whiteboard-section">
+        <h3 ref="elNotUrgentTitle" :style="style3" style="position: fixed">
+          Not Urgent
+        </h3>
+      </div>
+      <div class="whiteboard-section">
+        <h3 ref="elUrgentTitle" :style="style4" style="position: fixed">
+          Urgent
+        </h3>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style>
@@ -59,5 +104,16 @@ const switchTheme = () => {
 .dark {
   background-color: #222;
   color: #fff;
+}
+
+.whiteboard {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+}
+
+.whiteboard-section {
+  border: 1px solid #ccc;
+  min-height: 200px;
 }
 </style>
